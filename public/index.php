be131f640bbdb821a671e128be6b07f607e0e4ab
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2025-12-11 10:19:51 +0700

    auto update deploy

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../maman_core/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require __DIR__.'/../maman_core/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
